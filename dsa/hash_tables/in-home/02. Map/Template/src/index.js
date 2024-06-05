16c38d5bb8b65ca65525ebe2159c00196be7ee04
Nikola Nenovski <nicknenovski@gmail.com>
2024-06-06 00:21:05 +0300

    Map exercises

--- dsa/hash_tables/in-home/02. Map/Template/src/index.js
+++ dsa/hash_tables/in-home/02. Map/Template/src/index.js
@@ -1,1 +1,25 @@
-// test your structure here
+import { countOccurrences, groupBy } from './task.js';
+
+const occurrences = countOccurrences(['js', 'c#', 'js', 'c#', 'c++']);
+// console.log(occurrences);
+
+const cities = [
+  { country: 'BG', city: 'Sofia' },
+  { country: 'UK', city: 'London' },
+  { country: 'UK', city: 'Manchester' },
+  { country: 'BG', city: 'Plovdiv' },
+  { country: 'US', city: 'Chicago' },
+];
+
+// just create groups of objects:
+const byCountry = groupBy(cities, c => c.country);
+
+// create groups with specified values:
+const byCountry2 = groupBy(
+  cities,
+  c => c.country,
+  c => c.city
+);
+
+console.log(byCountry)
+console.log(byCountry2)
--- dsa/hash_tables/in-home/02. Map/Template/src/task.js
+++ dsa/hash_tables/in-home/02. Map/Template/src/task.js
@@ -1,1 +1,27 @@
-// solve the task here
+export const countOccurrences = array => {
+  const map = new Map();
+
+  for (const item of array) {
+    const count = map.has(item) || 0;
+    map.set(item, count + 1);
+  }
+
+  return map;
+};
+
+export const groupBy = (data, keySelector, valueSelector = x => x) => {
+  const map = new Map();
+
+  data.forEach(obj => {
+    const key = keySelector(obj);
+    const value = valueSelector(obj);
+
+    if (!map.has(key)) {
+      map.set(key, []);
+    }
+
+    map.get(key).push(value);
+  });
+
+  return map;
+};
