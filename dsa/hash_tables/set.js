ba7e7d9c158b35ec459ca07009562f8cbf158665
Nikola Nenovski <nicknenovski@gmail.com>
2024-06-05 21:48:53 +0300

    Symmetric difference function

--- dsa/hash_tables/set.js
+++ dsa/hash_tables/set.js
@@ -2,7 +2,7 @@
 const checkUniques = iterable => {
   const set = new Set(iterable);
   return set.size === iterable.length;
-}
+};
 const arrOfUniques = [1, 2, 3, 4, 5, 6];
 const arrOfNonUniques = [1, 2, 3, 4, 5, 6, 6, 6];
 console.log(checkUniques(arrOfUniques)); // true
@@ -28,8 +28,19 @@ const intersection = (first, second) => {
     if (first.has(item)) set.add(item);
   }
   return set;
-}
+};
 console.log(intersection(set1, set2)); // { 2, 3 }
 
-//5. Difference
-//6. Symmetric difference
+//5. Symmetric difference
+const array1 = [1, 2, 3, 4];
+const array2 = [3, 4, 5, 6];
+const symmetricDiff = (first, second) => {
+  const set1 = new Set(first);
+  const set2 = new Set(second);
+
+  const diff1 = Array.from(set1).filter(item => !set2.has(item));
+  const diff2 = Array.from(set2).filter(item => !set1.has(item));
+  return [...diff1, ...diff2];
+};
+
+console.log(symmetricDiff(array1, array2)); // [1, 2, 5, 6]
